433d5b0760861bcd281389c9be25bdd1c751cf96
shu-illy <sh.illy.9117@gmail.com>
2023-07-27 13:56:43 +0900

    force pushのエイリアス追加

--- .config/sheldon/defer/alias/global.zsh
+++ .config/sheldon/defer/alias/global.zsh
@@ -17,6 +17,7 @@ alias drmv='docker volume prune'
 alias g="git"
 alias gpso="git push origin"
 alias gps='current_branch=$(git rev-parse --abbrev-ref HEAD); [ "$current_branch" != "develop" ] && [ "$current_branch" != "main" ] && [ "$current_branch" != "develop" ] && git push origin "$current_branch"'
+alias gpsf='current_branch=$(git rev-parse --abbrev-ref HEAD); [ "$current_branch" != "develop" ] && [ "$current_branch" != "main" ] && [ "$current_branch" != "develop" ] && git push origin -f "$current_branch"'
 alias gpl="git pull origin"
 alias gplr="git pull --rebase --autostash origin"
 alias gco="git checkout"
