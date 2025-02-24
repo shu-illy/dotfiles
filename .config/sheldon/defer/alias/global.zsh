c13e625759ac3b8844f4b7302717fd66eb69256f
shu-illy <sh.illy.9117@gmail.com>
2025-02-24 18:16:55 +0900

    ghqのリポジトリ作成コマンド追加

--- .config/sheldon/defer/alias/global.zsh
+++ .config/sheldon/defer/alias/global.zsh
@@ -88,6 +88,36 @@ function r() {
     cd $(ghq root)/$src
   fi
 }
+# 新規リポジトリ作成してghq管理下に置く（引数でリポジトリ名指定）
+function ghq-new() {
+    local REPONAME=$1
+
+    if [ -z "$REPONAME" ]; then
+        echo 'Repository name must be specified.'
+        return
+    fi
+
+    local TMPDIR=/tmp/ghq_new
+    local TMPREPODIR=$TMPDIR/$REPONAME
+
+    mkdir -p $TMPREPODIR
+    cd $TMPREPODIR
+
+    git init
+    gh repo create $REPONAME --public --source=. --remote=origin
+
+    local REPOURL=$(git remote get-url origin)
+    local REPOPATH=$(echo $REPOURL | sed -e 's/^https:\/\///' -e 's/^git@//' -e 's/\.git$//' -e 's/github.com:/github.com\//')
+    local USER_REPO_NAME=$(echo $REPOPATH | sed -e 's/^github\.com\///')
+
+    ghq get $USER_REPO_NAME
+
+    cd $(ghq root)/$REPOPATH
+
+    rm -rf $TMPREPODIR
+}
+
+
 
 # 通知音
 alias beep='afplay /System/Library/Sounds/Ping.aiff'
