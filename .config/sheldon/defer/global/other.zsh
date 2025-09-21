134067e1d78a0ecd72916182144f4579038fb177
shu-illy <sh.illy.9117@gmail.com>
2025-09-22 01:42:22 +0900

    pdfminをglobalに移動

--- .config/sheldon/defer/global/other.zsh
+++ .config/sheldon/defer/global/other.zsh
@@ -53,3 +53,18 @@ function fzf-select-history() {
 }
 zle -N fzf-select-history
 bindkey '^r' fzf-select-history
+
+# GhostScriptでPDFを圧縮
+function pdfmin()
+{
+  local cnt=0
+  for i in $@; do
+    gs -sDEVICE=pdfwrite \
+      -dCompatibilityLevel=1.4 \
+      -dPDFSETTINGS=/ebook \
+      -dNOPAUSE -dQUIET -dBATCH \
+      -sOutputFile=${i%%.*}.min.pdf ${i} &
+    (( (cnt += 1) % 4 == 0 )) && wait
+  done
+  wait && return 0
+}
